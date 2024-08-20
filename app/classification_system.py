7f50346b3289986bdaafda9d65dead7275be4a67
Pradeep Yadav <pradeep.yadav@myrealdata.in>
2024-08-20 22:52:42 +0530

    Impact x Urgency = Priority added in response

--- app/classification_system.py
+++ app/classification_system.py
@@ -12,7 +12,7 @@ from ollama_instructor.ollama_instructor_client import OllamaInstructorClient
 
 '''
 Todo: 
-1. Add concept to choose the best response based on the confidence score returned by the different models.
+1. (done) Add concept to choose the best response based on the confidence score returned by the different models.
 2. We can add concept to check if ticket is fraud or not based on the other user PII data like IP, Country, etc.
 '''
 
@@ -20,8 +20,15 @@ class TicketCategory(str, Enum):
     ORDER_ISSUE = "order_issue"
     ACCOUNT_ACCESS = "account_access"
     PRODUCT_INQUIRY = "product_inquiry"
+    SALES_INQUIRY = "sales_inquiry"
     TECHNICAL_SUPPORT = "technical_support"
+    INFRASTRUCTURE = "infrastructure"
+    GENERAL_SUPPORT = "general_support"
     BILLING = "billing"
+    SHIPPING = "shipping"
+    FEEDBACK_SUGGESTIONS = "feedback_suggestions"
+    RETURN_REFUND = "return_refund"
+    FEATURE_REQUEST = "feature_request"
     OTHER = "other"
 
 
@@ -30,6 +37,8 @@ class CustomerSentiment(str, Enum):
     FRUSTRATED = "frustrated"
     NEUTRAL = "neutral"
     SATISFIED = "satisfied"
+    HAPPY = "happy"
+    CONFUSED = "confused"
 
 
 class TicketUrgency(str, Enum):
@@ -37,12 +46,39 @@ class TicketUrgency(str, Enum):
     MEDIUM = "medium"
     HIGH = "high"
     CRITICAL = "critical"
+    URGENT = "urgent"
 
+
+class TicketImpact(str, Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+    CRITICAL = "critical"
+
+
+class TicketPriority(str, Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+    CRITICAL = "critical"
+    IMMEDIATE = "immediate"
 
 class TicketClassification(BaseModel):
-    category: TicketCategory
-    urgency: TicketUrgency
-    sentiment: CustomerSentiment
+    category: TicketCategory = Field(
+        description="Category of the customer support ticket"
+    )
+    sentiment: CustomerSentiment = Field(    
+        description="Sentiment of the customer expressing the issue"
+    )
+    urgency: TicketUrgency = Field(
+        description="Urgency level for handling the ticket based on the customer's needs"
+    )
+    impact: TicketImpact = Field(
+        description="Impact of the ticket on the business operations"
+    )
+    priority: TicketPriority = Field(
+        description="Priority level for handling the ticket based on urgency and impact"
+    )
     confidence: float = Field(
         ge=0, le=1, description="Confidence score for the classification"
     )
@@ -57,9 +93,11 @@ class TicketClassification(BaseModel):
         json_schema_extra={
             "examples": [
                 {
-                    "category": "account_access",
-                    "urgency": "high",
-                    "sentiment": "frustrated",
+                    "category": TicketCategory.ACCOUNT_ACCESS,
+                    "sentiment": CustomerSentiment.FRUSTRATED,
+                    "urgency": TicketUrgency.HIGH,
+                    "impact": TicketImpact.CRITICAL,
+                    "priority": TicketPriority.CRITICAL,
                     "confidence": 0.9,
                     "key_information": ["pending orders", "password reset issue"],
                     "suggested_action": "Escalate to a senior support agent for further assistance with account access and pending order resolution.",
@@ -69,16 +107,6 @@ class TicketClassification(BaseModel):
     )
 
 
-ticket_classification = TicketClassification(
-    category=TicketCategory.ORDER_ISSUE,
-    urgency=TicketUrgency.HIGH,
-    sentiment=CustomerSentiment.ANGRY,
-    confidence=0.9,
-    key_information=["Order #12345", "Received tablet instead of laptop"],
-    suggested_action="Contact customer to arrange laptop delivery",
-)
-
-
 class ClassificationSystem:
     def __init__(self):
         self.system_prompt = f"""
@@ -86,17 +114,19 @@ class ClassificationSystem:
             Your role is to analyze incoming customer support tickets and provide structured information to help our team respond quickly and effectively
             
             Business Context:
-            - We handle thousands of tickets daily across various categories (orders, accounts, products, technical issues, billing).
+            - We handle thousands of tickets daily across various categories (orders, accounts, products, technical issues, sales, feedback,  billing).
             - Quick and accurate classification is crucial for customer satisfaction and operational efficiency.
             - We prioritize based on urgency and customer sentiment.
             
             Your tasks:
             1. Categorize the ticket into the most appropriate category.
-            2. Assess the urgency of the issue (low, medium, high, critical).
-            3. Determine the customer's sentiment.
-            4. Extract key information that would be helpful for our support team.
-            5. Suggest an initial action for handling the ticket.
-            6. Provide a confidence score for your classification.
+            2. Determine the customer's sentiment.
+            3. Assess the urgency of the issue (low, medium, high, critical, urgent).
+            4. Evaluate the impact on the business (low, medium, high, critical).
+            5. Assign a priority level for handling the ticket (low, medium, high, critical, immediate) based on the result of the combination of Impact and Urgency.
+            6. Extract key information that would be helpful for our support team.
+            7. Suggest an initial action for handling the ticket.
+            8. Provide a confidence score for your classification.
             
             Remember:
             - Be objective and base your analysis solely on the information provided in the ticket.
@@ -105,14 +135,12 @@ class ClassificationSystem:
             - The 'suggested_action' should be a brief, actionable step for our support team.
             Analyze the following customer support ticket and provide the requested information in the specified format while adhering to the following JSON schema: {TicketClassification.model_json_schema()}.
         """
-        self.os_primary_model = "llama3.1"
-        self.os_alternative_model = "mistral"
-        self.os_alternative_model_2 = "gemma:7b-instruct"
+        self.os_primary_models = ["llama3.1", "mistral", "gemma:7b-instruct"]
         self.paid_model_name = "gpt-4o"
         self.os_client = OllamaInstructorClient()
 
     def process_ticket(self, ticket_text: str, model_name: str) -> Mapping[str, Any]:
-        if model_name not in [self.os_primary_model, self.os_alternative_model, self.os_alternative_model_2]:
+        if model_name not in self.os_primary_models:
             raise ValueError(f"Invalid model name: {model_name}")
 
         print(f"Processing data using model: {model_name}")
@@ -131,16 +159,11 @@ class ClassificationSystem:
         )
     
     def classify(self, ticket_text: str) -> dict:
-        models = [
-            self.os_primary_model,
-            self.os_alternative_model,
-            self.os_alternative_model_2,
-        ]
         best_response = None
         highest_confidence = 0.0
         model_used = None
 
-        for model in models:
+        for model in self.os_primary_models:
             try:
                 response = self.process_ticket(ticket_text, model)
                 
